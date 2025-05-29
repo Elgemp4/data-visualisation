b66d011ba9e053ac727d3d73b05a4f80ad6af5f5
Emilien <marquegniesemilien@gmail.com>
2025-05-29 11:35:27 +0200

    Added hover effect

--- pie_svg.js
+++ pie_svg.js
@@ -2,6 +2,7 @@ const main = async () => {
     const colors = ["#FF5733", "#FFC300", "#3498DB", "#27AE60"];
 
     const pie_chart = document.querySelector(".pie_chart");
+    const pie_label = document.querySelector(".pie_label");
 
     const result = await fetch("/data.json");
 
@@ -25,17 +26,38 @@ const main = async () => {
             `A${radius} ${radius} 0 ${Math.abs(angle - previousAngle) > 180 ? 1 : 0} 1 ${endX} ${endY}`,
         ]
         const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
         path.setAttribute("d", pathData);
         path.setAttribute("fill", colors[index]);
-        console.log(path)
+        path.setAttribute("data-name", element.name);
+        path.setAttribute("class", `path-${element.name}`)
         pie_chart.appendChild(path);
+
+
+        pie_label.innerHTML+= `<li data-name=${element.name}>${element.name} (${element.value}%)</li>`
+
         index++;
         previousAngle = angle;
+    }
+
+    const dataNamesElements = document.querySelectorAll("[data-name]");
+
+    for(const element of dataNamesElements) {
+        element.addEventListener("mouseenter", (e) => {
+            const allElements = document.querySelectorAll(`[data-name=${element.dataset.name}]`);
+            console.log(allElements)
+            allElements.forEach((e) => e.classList.add("selected"));
+        })
+
+        element.addEventListener("mouseout", (e) => {
+            const allElements = document.querySelectorAll(`[data-name=${element.dataset.name}]`);
+            allElements.forEach((e) => e.classList.remove("selected"));
+        })
     }
 }
 
 const calculatePointOnCircle = (angle, centerX, centerY, radius) => {
-    const angleInRadians = angle * Math.PI / 180;
+    const angleInRadians = (angle - 90) * Math.PI / 180;
 
     const x = centerX + radius * Math.cos(angleInRadians);
     const y = centerY + radius * Math.sin(angleInRadians);
